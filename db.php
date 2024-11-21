a4b5fea23a1061a3a7f237864dbd7ca2a87bf3ef
phuongdd@educa.vn <phuongdd@educa.vn>
2024-11-21 14:20:21 +0700

    update user

--- db.php
+++ db.php
@@ -26,7 +26,7 @@ function checkEmailName($conn, $email, $name){
     $sql1 = "SELECT * from user WHERE email = '$email' AND `username` = '$name'";
 
     $result = $conn->query($sql1);
-    // var_dump($result);
+
     return $result->num_rows;
 
 }
